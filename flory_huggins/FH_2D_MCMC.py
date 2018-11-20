75e00d182cb2bb2a1055d9f194a0a7820f3ccd58
wtang8 <wai_shing_tang@brown.edu>
2018-11-20 11:57:13 -0500

    update MCMC

--- flory_huggins/FH_2D_MCMC.py
+++ flory_huggins/FH_2D_MCMC.py
@@ -11,66 +11,84 @@ pi = 3.14159265359
 kb = 1.38064852e-23 # kg m^2 / s^2 K
 na = 6.022140857e23 # Avogadro's number
 T = 35.05 # K
-beta = 1/kb/T/1e20
+#beta = 1/kb/T/1e20
+beta = 10
 
 boxlen = 1000.0 # Angstrom
 ngrid = 500
-niters = 10
 dx = boxlen / ngrid
 x = np.arange(0,boxlen,dx)
 
 rhob = 0.1
 nsp = 2
-rho = np.zeros((nsp*ngrid*ngrid),dtype=float)
-rho.fill(rhob)
+rho = np.zeros(nsp*ngrid,dtype=float)
+rho[0:int(np.floor(ngrid/2))] = rhob
+rho[-int(np.floor(ngrid/2)):] = rhob
+#rho.fill(rhob)
 xpol = np.array([50,50])
 chi = np.matrix('.2 .8; .8 .2')
-totaldensity = rhob*boxlen
+#print chi
+totaldensity = rhob*ngrid
 
-bhT = 0.01
-ftol = 0.00001
-gtol = 0.0001
+mciter = 1000
+mcstep = rhob/20
 
 lbnd = 0.000
 ubnd = 1.e99
-bnd = [(0.000,1.e99)]*(nsp*ngrid)
-stepsize = rhob/20
 
 #lmda = h/np.sqrt(2*pi*m*kb*T)*1e10 # Angstrom
-
 #print np.sum(rho,axis=0)
-normf = dx/totaldensity
+
 @jit#(parallel=True)
 def totalfe(rho):
     global ljr, ngrid
-    rho = np.array(list(zip(rho[:ngrid],rho[ngrid:2*ngrid]))) 
-    ratio = np.sum(rho,axis=0)*normf
-    rho /= ratio
     fent = 0.0
     for i in range(nsp):
         for x in range(ngrid):
-            if rho[x][i] > 0.0:
-                fent += rho[x][i]/xpol[i]*np.log(rho[x][i])
+            y = i*ngrid+x
+            if rho[y] > 0.0:
+                fent += rho[y]/xpol[i]*np.log(rho[y])
     fmix = 0.0
+    v = np.zeros(nsp,dtype=float)
     for x in range(ngrid):
-        fmix += rho[x].dot(chi.dot(rho[x]))
+        v[0] = rho[x]
+        v[1] = rho[ngrid+x] 
+        fmix += v.dot(chi.dot(v))
 #    print('%.3f %.3f'% (fent,fmix))
     return fent + fmix
-print totalfe(rho)
-#exit()
 
-res = optimize.basinhopping(totalfe,rho,T=bhT
-#    ,stepsize=stepsize
-    ,niter_success=niters
-    ,minimizer_kwargs={
-    'method':'L-BFGS-B',
-    'bounds':bnd,
-    'options':{
-    'gtol':gtol,
-    'ftol':ftol,
-    'disp': True}})
+feovertime = []
+def TakeStep(rho,p0):
+    x = rho
+    rho += np.random.uniform(-mcstep,mcstep,nsp*ngrid)
+    rho[rho < 0.0] = 0.0 
+    rho[:ngrid] *= totaldensity/np.sum(rho[:ngrid])
+    rho[ngrid:2*ngrid] *= totaldensity/np.sum(rho[ngrid:2*ngrid])
+    fe = totalfe(rho)
+    feovertime.append(fe)
+    p1 = np.exp(-fe)
+    if p1 > p0:
+#        print('accepted p1 = %.3e p0 = %.3e'%(p1,p0))
+        return rho, p1
+    else:
+        if np.random.uniform(0.0,1.0) < p1/p0:
+#            print('accepted p1/p0 = %.3e'%(p1/p0))
+            return rho, p1
+        else:
+#            print('rejected p1 = %.3e p0 = %.3e'%(p1,p0))
+            return x, p0
 
-np.savetxt('rho.dat',res.x)
+np.random.seed(0)
+plt.plot(x,rho[:ngrid],color='r')
+plt.plot(x,rho[ngrid:2*ngrid],color='b')
+plt.show()
+plt.close()
+print(totalfe(rho))
+p0 = np.exp(-totalfe(rho))
+for i in range(mciter):
+    rho, p0 = TakeStep(rho,p0)
+print(totalfe(rho))
+np.savetxt('rho.dat',rho)
 
 data = np.loadtxt('rho.dat')
 rho = data
@@ -79,3 +97,6 @@ plt.plot(x,rho[ngrid:2*ngrid],color='b')
 plt.show()
 plt.close()
 
+plt.plot(range(len(feovertime)),feovertime)
+plt.show()
+plt.close()
