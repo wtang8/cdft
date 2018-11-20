839b656921f0497c1102275d6e720adf2d1701a6
wtang8 <wai_shing_tang@brown.edu>
2018-11-20 15:28:09 -0500

    update FH_1D_MCMC.py

--- flory_huggins/FH_1D_MCMC.py
+++ flory_huggins/FH_1D_MCMC.py
@@ -12,7 +12,7 @@ kb = 1.38064852e-23 # kg m^2 / s^2 K
 na = 6.022140857e23 # Avogadro's number
 T = 35.05 # K
 #beta = 1/kb/T/1e20
-beta = 10
+beta = 20.0
 
 boxlen = 1000.0 # Angstrom
 ngrid = 500
@@ -25,13 +25,13 @@ rho = np.zeros(nsp*ngrid,dtype=float)
 rho[0:int(np.floor(ngrid/2))] = rhob
 rho[-int(np.floor(ngrid/2)):] = rhob
 #rho.fill(rhob)
-xpol = np.array([100,100])
+xpol = np.array([10,10])
 chi = np.matrix('0. .8; .8 0.')
 #print chi
 totaldensity = rhob*ngrid/2
 
 mciter = 1000
-mcstep = rhob/20
+mcstep = rhob/50
 
 lbnd = 0.000
 ubnd = 1.e99
@@ -57,43 +57,42 @@ def totalfe(rho):
 #    print('%.3f %.3f'% (fent,fmix))
     return fent + fmix
 
-acptrate = 0.0
 feovertime = []
-def TakeStep(rho,p0):
-    global acptrate
+def TakeStep(rho,p0,acpt,niter):
     x = rho
+    niter += 1
     rho += np.random.uniform(-mcstep,mcstep,nsp*ngrid)
     rho[rho < 0.0] = 0.0 
     rho[:ngrid] *= totaldensity/np.sum(rho[:ngrid])
     rho[ngrid:2*ngrid] *= totaldensity/np.sum(rho[ngrid:2*ngrid])
     fe = totalfe(rho)
-    feovertime.append(fe)
-    p1 = np.exp(-fe)
+    p1 = np.exp(-fe*beta)
     if p1 > p0:
-        acptrate += 1
-#        print('accepted p1 = %.3e p0 = %.3e'%(p1,p0))
-        return rho, p1
+        acpt += 1
+        feovertime.append(fe)
+        print('accepted p1 = %.3e p0 = %.3e'%(p1,p0))
+        return rho, p1, acpt, niter
     else:
         if np.random.uniform(0.0,1.0) < p1/p0:
-            acptrate += 1
-#            print('accepted p1/p0 = %.3e'%(p1/p0))
-            return rho, p1
+            acpt += 1
+            print('accepted p1 = %.3e p0 = %.3e'%(p1,p0))
+            return rho, p1, acpt, niter
         else:
-#            print('rejected p1 = %.3e p0 = %.3e'%(p1,p0))
-            return x, p0
+            print('rejected p1 = %.3e p0 = %.3e'%(p1,p0))
+            return x, p0, acpt, niter
 
 np.random.seed(0)
 plt.plot(x,rho[:ngrid],color='r')
 plt.plot(x,rho[ngrid:2*ngrid],color='b')
 plt.show()
 plt.close()
-print(totalfe(rho))
-p0 = np.exp(-totalfe(rho))
-for i in range(mciter):
-    rho, p0 = TakeStep(rho,p0)
-print(totalfe(rho))
-acptrate /= mciter
-print('Acceptance rate = %.3f' % acptrate)
+acpt = 0
+niter = 0
+fe = totalfe(rho)
+p0 = np.exp(-fe*beta)
+while niter < mciter:
+    rho, p0, acpt, niter = TakeStep(rho,p0,acpt,niter)
+print('Acceptance rate = %.3f' % (float(acpt)/niter))
 np.savetxt('rho.dat',rho)
 
 data = np.loadtxt('rho.dat')
